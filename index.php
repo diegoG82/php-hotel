3469aefed5e865f4cf606781c646383c0d697824
diegoG82 <diegogiardina@hotmail.it>
2023-05-04 18:10:31 +0200

    refactoring table values and added forms

--- index.php
+++ index.php
@@ -66,14 +66,45 @@ $hotels = [
 
 <body>
 
+
     <div class="container mt-4">
 
         <h1 class="text-center">Lista Hotel</h1>
 
+
+        <form action="index.php" method="GET">
+
+            <select class="form-select mb-4" name="stars" id="stars">
+                <option selected>Seleziona stelle Hotel</option>
+                <option value="1">1 stella</option>
+                <option value="2">2 stelle</option>
+                <option value="3">3 stelle</option>
+                <option value="4">4 stelle</option>
+                <option value="5">5 stelle</option>
+            </select>
+            <button class="mb-2" type="submit">cerca</button>
+
+        </form>
+
+
+        
+
+
+        <form action="index.php" method="GET">
+            <select class="form-select mb-4" aria-label="Default select example" name="parking" id="parking">
+                <option value="1">Con parcheggio</option>
+                <option value="2">Senza parcheggio</option>
+
+            </select>
+            <button class="mb-2 type=" submit">cerca</button>
+
+        </form>
+
+
         <table class="table table-dark">
             <thead>
                 <tr>
-                    <th scope="col">Position in array</th>
+                    <th scope="col">Position </th>
                     <th scope="col">Name</th>
                     <th scope="col">Description</th>
                     <th scope="col">Parking</th>
@@ -84,15 +115,19 @@ $hotels = [
 
             <tbody>
 
-                <?php foreach ($hotels as $key => $hotelist) { ?>
+                <?php foreach ($hotels as $key => $hotelist) {
+
+
+
+                ?>
 
                     <tr>
-                        <th scope="row"><?php echo $key ?></th>
+                        <th scope="row"><?php echo $key + 1 ?></th>
                         <th scope="row"><?php echo $hotelist['name'] ?></th>
                         <th scope="row"><?php echo $hotelist['description'] ?></th>
                         <th scope="row"><?php echo $hotelist['parking'] ? 'Yes' : 'No'; ?></th>
-                        <th scope="row"><?php echo $hotelist['vote'] ?></th>
-                        <th scope="row"><?php echo $hotelist['distance_to_center'] ?></th>
+                        <th scope="row"><?php echo $hotelist['vote'] ?> Stars</th>
+                        <th scope="row"><?php echo $hotelist['distance_to_center'] ?> km</th>
                     </tr>
 
                 <?php }
